be78149b619501aa1151b7ffc4a15633d83926a2
chenchuk77 <chenchuk@gmail.com>
2023-03-04 00:31:49 +0200

    adding windows client support

--- webserver.py
+++ webserver.py
@@ -6,10 +6,19 @@ import os
 
 app = Flask(__name__)
 
+# linux client expects client.conf
 @app.route('/client.conf')
 def download():
     path = 'client.conf'
     return send_file(path, as_attachment=True)
+
+
+# windows client must be different and needs the same file as *.ovpn
+@app.route('/client.ovpn')
+def download():
+    path = 'client.conf'
+    return send_file(path, attachment_filename='client.ovpn', as_attachment=True)
+
 
 if __name__ == '__main__':
     try:
